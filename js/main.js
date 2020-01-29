ec724897377717010b88da8e3f6883f744715ff9
Hassan Al-najjar <ha77an19@gmail.com>
2020-01-29 08:52:41 +0200

    buyer page #5

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let cart = document.getElementById("cart");
+let goToCart = document.getElementById("gotocart");
+let products = document.querySelector('.products');
+let cart__cancel = document.querySelector('.cart__cancel');
+goToCart.addEventListener("click" , gotocar);
+function gotocar() {
+  cart.style.display = "flex";
+  products.style.display = "none";
+};
+cart__cancel.addEventListener('click',backToProduct);
+function backToProduct(){
+    cart.style.display = "none";
+    products.style.display = "flex";
+};
