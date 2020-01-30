2430ad877124fae41a76c7c4cd06b993e3530e74
Lina Ebeid <linaebe0@gmail.com>
2020-01-30 10:05:08 +0200

    edit dom is done Relates #45

--- js/domseller.js
+++ js/domseller.js
@@ -14,13 +14,14 @@ hideForm.addEventListener("click", function() {
   addForm.style.display = "none";
 });
 
-const namePro = document.getElementById("product-name").value;
+
+
+function showProduct() {
+  const namePro = document.getElementById("product-name").value;
   const detPro = document.getElementById("product-detailes").value;
   const pricePro = document.getElementById("product-price").value;
   const imgPro = document.getElementById("product-image").value;
   const catPro = document.getElementById("product-category").value;
-
-function showProduct() {
   
   let product = JSON.parse(localStorage.getItem("product"));
   if (!product) {
